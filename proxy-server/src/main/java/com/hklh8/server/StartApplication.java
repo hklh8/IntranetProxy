b05128c8cfdf1761a80dc307f98eaf96c6289fe2
GouBo <gb@hklh8.com>
2018-02-05 00:37:10 +0800

    server端修改为spring boot模式。

--- proxy-server/src/main/java/com/hklh8/server/StartApplication.java
+++ proxy-server/src/main/java/com/hklh8/server/StartApplication.java
@@ -1,7 +1,10 @@
 package com.hklh8.server;
 
+import com.hklh8.common.container.ContainerHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 /**
  * Created by GouBo on 2018/2/4.
@@ -11,5 +14,7 @@ public class StartApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(StartApplication.class, args);
+
+        ContainerHelper.start(Arrays.asList(new ProxyServerContainer()));
     }
 }
---
+++ proxy-server/src/main/java/com/hklh8/server/controller/WebController.java
@@ -0,0 +1,117 @@
+package com.hklh8.server.controller;
+
+import com.google.gson.reflect.TypeToken;
+import com.hklh8.common.JsonUtil;
+import com.hklh8.server.ProxyChannelManager;
+import com.hklh8.server.config.ProxyConfig;
+import com.hklh8.server.dto.ResponseInfo;
+import com.hklh8.server.metrics.MetricsCollector;
+import io.netty.channel.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+public class WebController {
+
+    protected static final String AUTH_COOKIE_KEY = "token";
+
+    private static Logger logger = LoggerFactory.getLogger(WebController.class);
+
+    /**
+     * 管理员不能同时在多个地方登录
+     */
+    private static String token;
+
+    /**
+     * 获取配置详细信息
+     */
+    @ResponseBody
+    @RequestMapping("/config/detail")
+    public Object getConfig() {
+        List<ProxyConfig.Client> clients = ProxyConfig.getInstance().getClients();
+        for (ProxyConfig.Client client : clients) {
+            Channel channel = ProxyChannelManager.getCmdChannel(client.getClientKey());
+            if (channel != null) {
+                client.setStatus(1);// online
+            } else {
+                client.setStatus(0);// offline
+            }
+        }
+        return ResponseInfo.build(ProxyConfig.getInstance().getClients());
+    }
+
+    /**
+     * 更新配置
+     */
+    @ResponseBody
+    @RequestMapping("/config/update")
+    public Object updateConfig(@RequestBody String config) {
+        List<ProxyConfig.Client> clients = JsonUtil.json2object(config, new TypeToken<List<ProxyConfig.Client>>() {
+        });
+        if (clients == null) {
+            return ResponseInfo.build(ResponseInfo.CODE_INVILID_PARAMS, "Error json config");
+        }
+
+        try {
+            ProxyConfig.getInstance().update(config);
+        } catch (Exception ex) {
+            logger.error("config update error", ex);
+            return ResponseInfo.build(ResponseInfo.CODE_INVILID_PARAMS, ex.getMessage());
+        }
+
+        return ResponseInfo.build(ResponseInfo.CODE_OK, "success");
+    }
+
+    @ResponseBody
+    @RequestMapping("/login")
+    public Object login(@RequestBody String config) {
+        Map<String, String> loginParams = JsonUtil.json2object(config, new TypeToken<Map<String, String>>() {
+        });
+        if (loginParams == null) {
+            return ResponseInfo.build(ResponseInfo.CODE_INVILID_PARAMS, "Error login info");
+        }
+
+        String username = loginParams.get("username");
+        String password = loginParams.get("password");
+
+        if (username == null || password == null) {
+            return ResponseInfo.build(ResponseInfo.CODE_INVILID_PARAMS, "Error username or password");
+        }
+
+        if (username.equals(ProxyConfig.getInstance().getConfigAdminUsername()) && password.equals(ProxyConfig.getInstance().getConfigAdminPassword())) {
+            token = UUID.randomUUID().toString().replace("-", "");
+            return ResponseInfo.build(token);
+        }
+        return ResponseInfo.build(ResponseInfo.CODE_INVILID_PARAMS, "Error username or password");
+    }
+
+    @ResponseBody
+    @RequestMapping("/logout")
+    public Object logout() {
+        token = null;
+        return ResponseInfo.build(ResponseInfo.CODE_OK, "success");
+    }
+
+    /**
+     * 获取数据统计
+     */
+    @ResponseBody
+    @RequestMapping("/metrics/get")
+    public Object test4() {
+        return ResponseInfo.build(ResponseInfo.CODE_OK, "success");
+    }
+
+    @ResponseBody
+    @RequestMapping("/metrics/getandreset")
+    public Object test5() {
+        return ResponseInfo.build(MetricsCollector.getAndResetAllMetrics());
+    }
+}
